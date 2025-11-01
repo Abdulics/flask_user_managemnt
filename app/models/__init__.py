924e3738a4bec668a43cd13b2e883f39db9a7d0f
Abdul Omar <abdulics93@yahoo.com>
2025-10-31 20:04:42 -0500

    add imports for models in models/__init__.py for migrations to detect them properly.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,7 @@
-# models/__init__.py
 from .user import User
 from .team import Team
 from .task import Task
 from .employees import Employee
-from .address import Address  # <- import Address here
+from .address import Address
 from .timeoff import TimeOff
 from .attendance import Attendance
